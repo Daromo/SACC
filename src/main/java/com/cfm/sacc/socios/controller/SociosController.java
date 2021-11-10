38115b13c4e841e37e80c1e7557697e3cda39e7b
Daniel Rojas <danielrojas@gmail.com>
2021-11-09 19:42:34 -0600

    refactor: formulario porcentajes boton quitar

--- src/main/java/com/cfm/sacc/socios/controller/SociosController.java
+++ src/main/java/com/cfm/sacc/socios/controller/SociosController.java
@@ -5,6 +5,7 @@ import java.util.LinkedList;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.cfm.sacc.socios.model.Porcentaje;
@@ -12,12 +13,12 @@ import com.cfm.sacc.socios.model.Porcentaje;
 @Controller
 @RequestMapping(value = "/socios")
 public class SociosController {
-	
+	private static final String PATH_FORM_PORCENTAJES = "socios/formPorcentajes";
 	LinkedList<Porcentaje> detallesPorcentajes = new LinkedList<>();
 	
 	@GetMapping("/porcentajes")
 	public String renderFormPorcentajes(Porcentaje porcentaje) {
-		return "socios/formPorcentajes";
+		return PATH_FORM_PORCENTAJES;
 	}
 
 	@GetMapping("/agregar/porcentaje")
@@ -25,14 +26,21 @@ public class SociosController {
 		boolean socioIsPresent = false;
 		for(Porcentaje item : detallesPorcentajes) {
 			if (porcentaje.getRfcSocio().equals(item.getRfcSocio()))
-				socioIsPresent = true;
+				socioIsPresent = true;				
 		}
 		
 		if(!socioIsPresent)
 			this.detallesPorcentajes.add(porcentaje);
 		
 		model.addAttribute("listaPorcentajes", detallesPorcentajes);
-		return "socios/formPorcentajes";
+		return PATH_FORM_PORCENTAJES;
 	}
-		
+	
+	@GetMapping("/remover/porcentaje/{rfc}")
+	public String removerDetallePorcentaje(@PathVariable String rfc,Porcentaje porcentaje, Model model) {
+		if(detallesPorcentajes.removeIf(item -> item.getRfcSocio().equals(rfc)))
+			model.addAttribute("listaPorcentajes", detallesPorcentajes);
+		return PATH_FORM_PORCENTAJES;
+	}
+	
 }
