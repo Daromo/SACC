0f235a9ef8e1317e85c278151c5c354ae0fd5549
Daniel Rojas <danielrojas@gmail.com>
2021-11-11 17:49:08 -0600

    refactor: formPorcentajes

--- src/main/java/com/cfm/sacc/socios/controller/SociosController.java
+++ src/main/java/com/cfm/sacc/socios/controller/SociosController.java
@@ -7,8 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.cfm.sacc.socios.model.Porcentaje;
 import com.cfm.sacc.socios.model.Socio;
@@ -24,6 +26,7 @@ public class SociosController {
 	@Autowired
 	ISocioService socioService;
 	
+	//Obtener los registros de los socios desde el servicio
 	@GetMapping("/activos")
 	public String renderListSocios(Model model) {
 		List<Socio> listaSocios = socioService.getListaSocios();
@@ -31,41 +34,53 @@ public class SociosController {
 		return "socios/listSocios";
 	}
 	
+	//RENDERIZAR EL FORMULARIO DE PORCENTAJES
 	@GetMapping("/porcentajes")
-	public String renderFormPorcentajes(Porcentaje porcentaje) {
-		return PATH_FORM_PORCENTAJES;
-	}
-
-	@GetMapping("/agregar/porcentaje")
-	public String agregarDetallePorcentaje(Porcentaje porcentaje, Model model) {
-		boolean socioIsPresent = false;
-		for(Porcentaje item : detallesPorcentajes) {
-			if (porcentaje.getRfcSocio().equals(item.getRfcSocio()))
-				socioIsPresent = true;
-		}
-		
-		if(!socioIsPresent)
-			this.detallesPorcentajes.add(porcentaje);
-		model.addAttribute("listaPorcentajes", detallesPorcentajes);
+	public String renderFormPorcentajes(Porcentaje porcentaje, Model model) {
+		List<Socio> listaSocios = socioService.getListaSocios();
+		model.addAttribute("socios", listaSocios);
 		return PATH_FORM_PORCENTAJES;
 	}
 	
-	@GetMapping("/remover/porcentaje/{rfc}")
+	/*
+	 * METODO PARA AGREGAR EL PORCENTAJE A LA TABLA DE DETALLES
+	 */
+	@GetMapping("/porcentaje/agregar/detalle")
+	public String agregarDetallePorcentaje(Porcentaje porcentaje, Model model, RedirectAttributes redirectAtrAttributes) {
+		for(Porcentaje item : detallesPorcentajes) {
+			if (porcentaje.getRfcSocio().equals(item.getRfcSocio())) {
+				redirectAtrAttributes.addFlashAttribute("settings", "¡El cliente ya existe!");
+				return "redirect:/socios/porcentajes";
+			}
+		}
+		this.detallesPorcentajes.add(porcentaje);
+		return PATH_FORM_PORCENTAJES;
+	}
+	
+	/*
+	 * METODO PARA ELIMINAR PORCENTAJE DESDE LA LISTA 
+	 */
+	@GetMapping("/porcentaje/remover/{rfc}")
 	public String removerDetallePorcentaje(@PathVariable String rfc,Porcentaje porcentaje, Model model) {
 		if(detallesPorcentajes.removeIf(item -> item.getRfcSocio().equals(rfc)))
 			model.addAttribute("listaPorcentajes", detallesPorcentajes);
 		return PATH_FORM_PORCENTAJES;
 	}
 	
-	@GetMapping("/porcentajes/cancelar")
+	@GetMapping("/porcentaje/cancelar")
 	public String cancelarPorcentajes(Porcentaje porcentaje) {
 		detallesPorcentajes.clear();
 		return PATH_FORM_PORCENTAJES;
 	}
 	
+	@GetMapping("/porcentaje/guardar")
+	public String agregarPorcentaje() {
+		return null;
+	}
 	
-	/*@ModelAttribute
+	@ModelAttribute
 	public void setGenericos(Model model) {
+		model.addAttribute("listaPorcentajes", detallesPorcentajes);
 		model.addAttribute("socios", socioService.getListaSocios());
-	}*/
+	}
 }
