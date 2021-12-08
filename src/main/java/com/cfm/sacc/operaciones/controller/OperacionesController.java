2c55357132d25dd6134abc32244ed4de29491fe0
Daniel Rojas <danielrojas@gmail.com>
2021-12-07 19:20:09 -0600

    refactor: generar recibo honorario

--- src/main/java/com/cfm/sacc/operaciones/controller/OperacionesController.java
+++ src/main/java/com/cfm/sacc/operaciones/controller/OperacionesController.java
@@ -1,6 +1,11 @@
 package com.cfm.sacc.operaciones.controller;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,6 +17,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.cfm.sacc.clientes.model.Cliente;
 import com.cfm.sacc.clientes.service.IClienteService;
 import com.cfm.sacc.operaciones.model.ReciboHonorario;
+
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.view.JasperViewer;
 
 @Controller
 @RequestMapping("/operaciones")
@@ -27,8 +41,19 @@ public class OperacionesController {
 		return "operaciones/formGenerarRecibo";
 	}
 	
-	@PostMapping("/recibo/guardar")
-	public String guardar(ReciboHonorario reciboHonorario) {
+	@PostMapping("/guardar-recibo")
+	public String guardar(ReciboHonorario reciboHonorario) throws FileNotFoundException, JRException {
+		reciboHonorario.setId(0000);
+		Map<String, Object> parameters = new  HashMap<String, Object>();
+		List<ReciboHonorario> recibo = Arrays.asList(reciboHonorario);
+		JasperReport compileReport = JasperCompileManager.compileReport(new FileInputStream("src/main/resources/report_templates/recibo_honorario.jrxml"));
+		JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(recibo);
+		
+		JasperPrint print = JasperFillManager.fillReport(compileReport, parameters, dataSource);
+		JasperViewer.viewReport(print, false);
+		//ERROR
+		JasperExportManager.exportReportToPdfFile(print, "honorarios.pdf");
+		
 		return "operaciones/formGenerarRecibo";
 	}
 }
--- src/main/java/com/cfm/sacc/operaciones/model/ReciboHonorario.java
+++ src/main/java/com/cfm/sacc/operaciones/model/ReciboHonorario.java
@@ -1,14 +1,18 @@
 package com.cfm.sacc.operaciones.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 @Getter
 @Setter
+@AllArgsConstructor
 @ToString
 public class ReciboHonorario {
+	private Integer id;
 	private String clienteRFC;
+	private String nombreCliente;
 	private Double importeHonorario;
 	private String importeLetra;
 	private String concepto;
