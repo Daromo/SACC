74a1adca08067c8490ce31ca92e60caaeadb92ea
Daniel Rojas <danielrojas@gmail.com>
2021-12-12 23:42:26 -0600

    fix: Errores en las fechas
    Correcion en los formatos de las fechas para agreagr y actualizar los datos de un cliente

--- src/main/java/com/cfm/sacc/SaccApplication.java
+++ src/main/java/com/cfm/sacc/SaccApplication.java
@@ -1,11 +1,19 @@
 package com.cfm.sacc;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 @SpringBootApplication
 public class SaccApplication {
@@ -16,11 +24,22 @@ public class SaccApplication {
 	    
 	@Bean
 	public ObjectMapper objectMapper() {
-		return new ObjectMapper();
+		final ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.registerModule(new JavaTimeModule());
+		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd"); 
+		dateFormat.setTimeZone(TimeZone.getTimeZone("America/Mexico_City")); 
+		objectMapper.setDateFormat(dateFormat);
+		objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+		return objectMapper;
 	}
 	
 	@Bean
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
+	}
+	
+	@PostConstruct
+	void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Mexico_City"));
 	}
 }
--- src/main/java/com/cfm/sacc/clientes/model/Cliente.java
+++ src/main/java/com/cfm/sacc/clientes/model/Cliente.java
@@ -3,6 +3,8 @@ package com.cfm.sacc.clientes.model;
 import java.util.Date;
 import java.util.Map;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.Getter;
@@ -20,9 +22,11 @@ public class Cliente {
 	private String apPaternoCliente;
 	private String apMaternoCliente;
 	private String razonSocial;
-	@JsonFormat(pattern = "yyyy-MM-dd")
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone =  "America/Mexico_City")
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date fechaIngreso;
-	@JsonFormat(pattern = "yyyy-MM-dd")
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "America/Mexico_City")
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date lastUpdate;
 	private Character status;
 	private String telefono1;
